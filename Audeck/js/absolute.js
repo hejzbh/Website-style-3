4dc5301b8ea43e60230ca0885f100bd022e5ee4a
hejzbh <63502201+hejzbh@users.noreply.github.com>
2022-05-23 22:50:01 +0200

    Delete Audeck directory

--- Audeck/js/absolute.js
+++
@@ -1,7 +0,0 @@
-const search = document.querySelector('#search-bar')
-const abs = document.querySelector('.absolute-section')
-
-search.addEventListener('click', function() {
-search.classList.toggle('is-active')
-abs.classList.toggle('active')
-})
--- Audeck/js/close.js
+++
@@ -1,7 +0,0 @@
-const zatvori = document.querySelector('#close')
-const sec = document.querySelector('.absolute-section')
-
-zatvori.addEventListener('click', function() {
-zatvori.classList.toggle('is-active')
-sec.classList.toggle('active')
-})
